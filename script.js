4da6ec66d7d9e95cc78444256a2a559580b8dde7
baba zone <168071227+alexverma72@users.noreply.github.com>
2026-04-29 16:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,7 @@
-let selectedFile;
-
-// file select
-document.getElementById("fileInput").addEventListener("change", (e) => {
-  selectedFile = e.target.files[0];
-  document.getElementById("fileName").innerText = selectedFile.name;
-});
-
-// drag drop
-const dropArea = document.getElementById("dropArea");
-
-dropArea.ondragover = (e) => {
-  e.preventDefault();
-};
-
-dropArea.ondrop = (e) => {
-  e.preventDefault();
-  selectedFile = e.dataTransfer.files[0];
-  document.getElementById("fileName").innerText = selectedFile.name;
-};
-
-// COMPRESS API
-async function compressPDF() {
-
-  if (!selectedFile) return alert("File select karo");
-
-  const apiKey = "alexverma72@gmail.com_fh0K1ep0zjRqqAGMVghcIWhmvosbde9zlowF5ub4DREfJGJFsLD0LFuBT2LJcOjF"; // 🔴 yaha new key daalna
-
-  const formData = new FormData();
-  formData.append("file", selectedFile);
-
-  document.getElementById("loader").style.display = "block";
-
-  try {
-    const response = await fetch("https://api.pdf.co/v1/pdf/optimize", {
-      method: "POST",
-      headers: {
-        "x-api-key": apiKey
-      },
-      body: formData
-    });
-
-    const data = await response.json();
-
-    document.getElementById("loader").style.display = "none";
-
-    if (data.url) {
-      window.open(data.url, "_blank");
-    } else {
-      alert("Error: " + data.message);
-    }
-
-  } catch (error) {
-    document.getElementById("loader").style.display = "none";
-    alert("Network error");
-    console.error(error);
-  }
-  function openTool(tool) {
+function openTool(tool) {
 
   if (tool === "compress") {
-    window.location.href = "index.html";
+    window.location.href = "tools/compress.html";
   }
 
   else if (tool === "merge") {
@@ -69,7 +12,4 @@ async function compressPDF() {
     window.location.href = "tools/pdf-to-jpg.html";
   }
 
-  else {
-    alert(tool + " coming soon");
-  }
 }
